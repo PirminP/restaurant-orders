a8c7c4a73a7013cf15788c671c379fcc38a5be06
PirminP <pirmin.pernsteiner@gmail.com>
2023-06-20 19:09:56 -0300

    feat task3: Implementing methods init and load menu data for class MenuData

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,18 @@
-# Req 3
+import csv
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self.load_menu_data(source_path)
+
+    def load_menu_data(self, source_path):
+        with open(source_path, 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            self.process_menu_data(reader)
+
+    def process_menu_data(self, reader):
+        for row in reader:
+            ...
+            # search for row data dish name, price, ingredients, amount
